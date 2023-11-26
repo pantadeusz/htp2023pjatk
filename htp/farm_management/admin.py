df80ac0c99e04c92604a3a75a472a511ebfe495f
plczapiewski <plczapiewski@gmail.com>
2023-11-26 08:45:48 +0100

    Add measurement model to admin panel

--- htp/farm_management/admin.py
+++ htp/farm_management/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Farm, FarmActivity
+from .models import Farm, FarmActivity, Measurement, Enum
 
 
 # Register your models here.
 admin.site.register(Farm)
 admin.site.register(FarmActivity)
+admin.site.register(Measurement)
+admin.site.register(Enum)
